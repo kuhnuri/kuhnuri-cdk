80e7aa384534a920049da3b9d471b29c9cdbdced
Jarno Elovirta <jarno@elovirta.com>
2019-07-07 11:25:01 +0300

    Resolve job status
    Signed-off-by: Jarno Elovirta <jarno@elovirta.com>

--- lambda/query.ts
+++ lambda/query.ts
@@ -1,6 +1,7 @@
 import { APIGatewayEvent } from "aws-lambda";
 import { DynamoDB } from "aws-sdk";
 import { readEnv, error, response } from "./utils";
+import { Job } from "./types";
 
 export async function handler(event: APIGatewayEvent) {
   if (!event.pathParameters || event.pathParameters.id === "") {
@@ -20,7 +21,8 @@ export async function handler(event: APIGatewayEvent) {
     const res = await dynamo.get(query).promise();
 
     if (res.Item) {
-      return response(200, res.Item);
+      const job = resolveStatus(res.Item as Job);
+      return response(200, job);
     } else {
       return error(404, `Job not found: ${jobId}`);
     }
@@ -28,4 +30,19 @@ export async function handler(event: APIGatewayEvent) {
     console.error(err);
     return error(500, `Failed to query job state: ${err}`);
   }
+}
+
+function resolveStatus(job: Job): Job {
+  const error = job.transtype.find(task => task.status === "error");
+  if (error) {
+    job.status = "error";
+    job.finished = error.finished;
+  } else if (job.transtype.every(task => task.status === "done")) {
+    job.status = "done";
+    job.finished = job.transtype[job.transtype.length - 1].finished;
+  } else if (job.transtype.find(task => task.status === "process")) {
+    job.status = "process";
+  }
+
+  return job;
 }
